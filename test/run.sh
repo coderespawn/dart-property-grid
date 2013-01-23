6fa9b5c346b0317dd532886b4d5c2a80aeac006d
Ali Akbar Vathi <ali.akbar@gmail.com>
2013-01-24 01:05:37 +0300

    Update test/run.sh

--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,31 @@
-#!/bin/sh
-DumpRenderTree test_runner.html | grep "CONSOLE"
+#!/bin/bash
 
+set -e
 
+#####
+# Unit Tests
+
+echo "DumpRenderTree test/index.html"
+results=`DumpRenderTree test/index.html 2>&1`
+
+echo "$results" | grep CONSOLE
+
+echo $results | grep 'unittest-suite-success' >/dev/null
+
+echo $results | grep -v 'Exception: Some tests failed.' >/dev/null
+
+#####
+# Type Analysis
+
+echo
+echo "dart_analyzer lib/*.dart"
+
+results=`dart_analyzer lib/*.dart 2>&1`
+
+echo "$results"
+
+if [ -n "$results" ]; then
+    exit 1
+else
+    echo "Passed analysis."
+fi
